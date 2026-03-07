55c990333d0d005957d29b857ab7af4d9c858658
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-03-07 21:33:56 +0530

    Code with basic authentication and authorization added

---
+++ Order-management-system/src/main/java/com/order_management/Order_management_system/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.order_management.Order_management_system.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
+
+@EnableWebSecurity
+@Configuration
+
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService users()
+    {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails trader = User.builder()
+                .username("trader")
+                .password(passwordEncoder().encode("trader123")).
+                roles("TRADER")
+                .build();
+
+        UserDetails viewer = User.builder()
+                .username("viewer")
+                .password(passwordEncoder().encode("viewer123"))
+                .roles("VIEWER")
+                .build();
+        return new InMemoryUserDetailsManager(admin,trader,viewer);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.csrf().disable();
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/oms/reports/**").hasAnyRole("ADMIN", "TRADER", "VIEWER")
+                .requestMatchers(HttpMethod.POST, "/oms/orders/create").hasAnyRole("TRADER", "ADMIN")
+                .requestMatchers(HttpMethod.GET, "/oms/orders/all").hasAnyRole("ADMIN", "TRADER")
+                .requestMatchers(HttpMethod.DELETE, "/oms/orders/*").hasAnyRole("ADMIN")
+                .requestMatchers("/oms/member/**").hasRole("ADMIN").anyRequest().authenticated())
+                .httpBasic();
+
+        return http.build();
+
+    }
+}
